d5e420405b517097cb6ab875263f61e8e29f3a42
applenick <applenick@users.noreply.github.com>
2021-10-25 18:25:22 -0700

    Pass optional target to obs interact event
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/matchshare/listeners/ObserverListener.java
+++ src/main/java/tc/oc/occ/matchshare/listeners/ObserverListener.java
@@ -1,10 +1,13 @@
 package tc.oc.occ.matchshare.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.inventory.ItemStack;
 import tc.oc.occ.dispense.events.observers.PGMObserverInteractEvent;
 import tc.oc.occ.dispense.events.observers.PGMObserverKitEvent;
 import tc.oc.occ.matchshare.MatchShare;
+import tc.oc.pgm.api.player.MatchPlayer;
 import tc.oc.pgm.api.player.event.ObserverInteractEvent;
 import tc.oc.pgm.spawns.events.ObserverKitApplyEvent;
 
@@ -17,8 +20,12 @@ public class ObserverListener extends ShareListener {
   @EventHandler
   public void onInteract(ObserverInteractEvent event) {
     if (event.getClickedItem() == null) return;
-    callNewEvent(
-        new PGMObserverInteractEvent(event.getPlayer().getBukkit(), event.getClickedItem()));
+    Player observer = event.getPlayer().getBukkit();
+    ItemStack itemHeld = event.getClickedItem();
+    MatchPlayer targetMatchPlayer = event.getClickedPlayer();
+    Player target = targetMatchPlayer != null ? targetMatchPlayer.getBukkit() : null;
+    boolean isTargetObserver = targetMatchPlayer != null && targetMatchPlayer.isObserving();
+    callNewEvent(new PGMObserverInteractEvent(observer, itemHeld, target, isTargetObserver));
   }
 
   @EventHandler(priority = EventPriority.MONITOR)
