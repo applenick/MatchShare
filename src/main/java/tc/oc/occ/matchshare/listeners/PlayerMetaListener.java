febc37ca3541a285790b33875eccf383ac8f97d9
applenick <applenick@users.noreply.github.com>
2021-11-07 18:12:19 -0800

    Track nickname status in player meta
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/matchshare/listeners/PlayerMetaListener.java
+++ src/main/java/tc/oc/occ/matchshare/listeners/PlayerMetaListener.java
@@ -2,8 +2,11 @@ package tc.oc.occ.matchshare.listeners;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import tc.oc.occ.matchshare.MatchShare;
+import tc.oc.pgm.api.integration.Integration;
 import tc.oc.pgm.api.party.Competitor;
 import tc.oc.pgm.events.PlayerJoinPartyEvent;
 
@@ -12,9 +15,18 @@ public class PlayerMetaListener extends ShareListener {
   public static final String OBS_KEY = "isObserving";
   public static final String PLAY_KEY = "isPlaying";
   public static final String MAP_KEY = "map";
+  public static final String NICK_KEY = "isNicked";
 
   public PlayerMetaListener(MatchShare plugin) {
     super(plugin);
+  }
+
+  @EventHandler(priority = EventPriority.MONITOR)
+  public void onJoinServer(PlayerJoinEvent event) {
+    boolean nicked = Integration.getNick(event.getPlayer()) != null;
+    if (nicked) {
+      event.getPlayer().setMetadata(NICK_KEY, new FixedMetadataValue(plugin, true));
+    }
   }
 
   @EventHandler
