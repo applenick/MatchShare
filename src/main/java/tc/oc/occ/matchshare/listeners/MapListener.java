582d4f8804c873e9ab2446653e1bfe98c96c08f2
applenick <applenick@users.noreply.github.com>
2023-09-11 11:13:17 -0700

    Support gamemode tracking on map load
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/matchshare/listeners/MapListener.java
+++ src/main/java/tc/oc/occ/matchshare/listeners/MapListener.java
@@ -1,7 +1,14 @@
 package tc.oc.occ.matchshare.listeners;
 
+import static net.kyori.adventure.text.Component.text;
+
 import com.google.common.collect.Sets;
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import tc.oc.occ.dispense.events.map.PGMMapListEvent;
@@ -10,10 +17,13 @@ import tc.oc.occ.dispense.events.match.MatchStatus;
 import tc.oc.occ.dispense.events.match.PGMMatchStatusUpdateEvent;
 import tc.oc.occ.matchshare.MatchShare;
 import tc.oc.pgm.api.PGM;
+import tc.oc.pgm.api.map.Gamemode;
 import tc.oc.pgm.api.map.MapInfo;
 import tc.oc.pgm.api.match.MatchPhase;
 import tc.oc.pgm.api.match.event.MatchLoadEvent;
 import tc.oc.pgm.api.match.event.MatchPhaseChangeEvent;
+import tc.oc.pgm.util.text.TextFormatter;
+import tc.oc.pgm.util.text.TextTranslations;
 
 public class MapListener extends ShareListener {
 
@@ -34,7 +44,25 @@ public class MapListener extends ShareListener {
     String name = map.getName();
     String desc = map.getDescription();
     int maxPlayers = map.getMaxPlayers().stream().reduce(0, Integer::sum);
-    callNewEvent(new PGMMapLoadEvent(name, desc, maxPlayers));
+    String gamemode = "";
+
+    TextComponent.Builder gmc = text();
+    Collection<Gamemode> gamemodes = map.getGamemodes();
+    if (map.getGamemode() != null) {
+      gmc.append(map.getGamemode());
+    } else if (!gamemodes.isEmpty()) {
+      boolean acronyms = gamemodes.size() > 1;
+      gmc.append(
+          TextFormatter.list(
+              gamemodes.stream()
+                  .map(gm -> text(acronyms ? gm.getAcronym() : gm.getFullName()))
+                  .collect(Collectors.toList()),
+              NamedTextColor.AQUA));
+    }
+
+    gamemode = TextTranslations.translateLegacy(gmc.build(), Bukkit.getConsoleSender());
+
+    callNewEvent(new PGMMapLoadEvent(name, desc, gamemode, maxPlayers));
   }
 
   @EventHandler
